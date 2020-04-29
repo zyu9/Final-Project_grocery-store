92578931233f25550d3d42b4b1374b9864984a8a
zyu9 <47763062+zyu9@users.noreply.github.com>
2020-04-29 18:02:03 -0400

    Add files via upload

---
+++ products/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+# /products -> index
+# Uniform Resource Locator (Address)
+def index(request):
+  products = Product.objects.all()
+  return render(request, 'index.html', {'products': products})
+
+
+def new(request):
+  return HttpResponse('New Products')
+
+
